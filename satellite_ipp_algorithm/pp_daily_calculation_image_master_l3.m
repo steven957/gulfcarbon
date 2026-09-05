ba649a1e273a4e15d26e8224f884a1f48f6c8033
steven957 <75032664+steven957@users.noreply.github.com>
2026-09-05 12:37:52 -0400

    Hyperspectral IPP algorithm scripts

--- satellite_ipp_algorithm/pp_daily_calculation_image_master_l3.m
+++ satellite_ipp_algorithm/pp_daily_calculation_image_master_l3.m
@@ -7,9 +7,8 @@
 % produce a water column primary production values for each pixel. 
 %
 % Inputs:
-%   1) Folder location with 'ed_calculation_with_satellite_par_input_single_file.m'
-%   2) Folder location with 'read_pace_kd490_PAR_aph442_netcdf_single_file.m' script
-%           or 'read_pace_l3_kd490_PAR_aph442_netcdf_single_file.m' script
+%   1) Folder location with 'ed_calculation_with_satellite_par_input_single_file.m' script
+%   2) Folder location with 'read_l3_kd490_PAR_aph440_netcdf_single_file' script
 %    
 % Outputs:
 %   1) Figures with plotted results
@@ -20,13 +19,19 @@
 %   2) ed_calculation_with_satellite_par_input_single_file.m - calculates spectral
 %   downwelling irradiance and PAR and PUR as a function of depth
 % 
-% MAT-files required: None
+% MAT-files required (files required for ed_calculation script): 
+%   1) 'ed0_es_shape_vector.mat' - shape vector for surface spectral irradiance
+%   2) 'fmicro_aph_dat.mat' - shape vector for microphytoplankton absorption
+%   3) 'fpiconano_aph_dat.mat' - shape vector for pico- and nanophytoplankton absorption
+%   4) 'kd_cluster_averages.mat' - mean diffuse spectral attenuation
+%          coefficients for downwelling irradiance for different water mass
+%          types (see Mili and Lohrenz, 2026)
 %
 % Author: Steven E. Lohrenz
 % School for Marine Science and Technology, University of Massachusetts Dartmouth
 % email address: slohrenz@umassd.edu
 % Website: http://www.umassd.edu/smast/
-% Last revision: 13 Aug 2026
+% Last revision: 5 Sep 2026
 
 %% ------------- BEGIN CODE --------------%% 
 
@@ -34,6 +39,7 @@ clc
 clearvars
 
 % Load file information
+% codepath = '/home/user/matlab/'; % Path to matlab code
 codepath = '/home/slohrenz/ocean_color/matlab/';
 
 % Specify number of depth intervals for calculations
@@ -43,6 +49,7 @@ codepath = '/home/slohrenz/ocean_color/matlab/';
 depth_n = 10; %length(z);
 
 % Get number of files to be read
+%labeltext=cellstr(input_file(1:12));
 
 sensor_type = 'PACE'; % 'MODIS'; % 'PACE'
 
@@ -65,17 +72,16 @@ for mnthn = monthrange
         prod_type = prod_flag{iprod};
         sensor_type = sensor_type; % 'MODIS'; % 'PACE'
         
-        % List of input files for IOP and PAR
+        % Get list of input files for IOP and PAR and read in data
         switch sensor_type
             case 'PACE'
-                inputfolder=['/run/media/slohrenz/PrimaryDrive/ocean_color/pace/','L3_',prod_type,'/'];
-                file_lst=dir([inputfolder,'PACE_OCI.2025',sprintf('%02d', mnthn),'*_',prod_type,'.L3.nc']);
+                inputfolder=['/home/user/pace/','L3_',prod_type,'/']; % Path to imagery data products
+                file_lst=dir([inputfolder,'L3_',prod_type,'/','PACE_OCI.2025',sprintf('%02d', mnthn),'*_',prod_type,'.L3.nc']);
                 iop_list = {prod_type};
                 % Load data
                 fname=char(file_lst.name);
 
-                %labeltext=cellstr(input_file(1:12));
-                filepath=[inputfolder,fname];
+                filepath = [inputfolder,'L3_',prod_type,'/',fname];
                 finfo = ncinfo(filepath);
 
                 disp(['Reading image data from ',fname]);
@@ -85,19 +91,19 @@ for mnthn = monthrange
                     run([codepath,'read_l3_kd490_PAR_aph440_netcdf_single_file']);
                 end
 
-            case 'MODIS'
-                if strcmp(prod_type,'OC')
+            case 'MODIS'                
+                if strcmp(prod_type,'OC') 
                     iop_list = {'kd','chlor_par'};
                 elseif strcmp(prod_type,'aph')
                     iop_list = {'aph'};
-                end
-                inputfolder='/run/media/slohrenz/PrimaryDrive/ocean_color/modis/';
+                end% Transpose to match dimensions of PUR
+
+                inputfolder='/home/user/modis/';  % Path to imagery data products
                 file_lst=dir([inputfolder,'A2025',sprintf('%02d', mnthn),'*_',prod_type,'.L3.nc']);
 
                 % Load data
                 fname=char(file_lst.name);
 
-                %labeltext=cellstr(input_file(1:12));
                 filepath=[inputfolder,fname];
                 finfo = ncinfo(filepath);
 
@@ -107,9 +113,7 @@ for mnthn = monthrange
                     iop = iop_list{ivar};
                     run([codepath,'read_l3_kd490_PAR_aph440_netcdf_single_file']);
                 end
-
         end
-    
     end
     
     %% Get solar elevation and azimuth for photoperiod
@@ -130,90 +134,72 @@ for mnthn = monthrange
     cos_solzen_air = cosd(sol_zen1);
     utc_h_from_midnght = mod((0:23.98),24);
     cos_indx = find(cos_solzen_air>0);
-    % sun_indx = find(cos_sEl > 0);
     
     solzen2_test = asind(sind(acosd(cos_solzen_air))./1.34); 
     
-    % Apply Snell's law to get transmitted angle across air-water
-    % interface
+    % Apply Snell's law to get transmitted angle across air-water interface and compute cosine (cos_solzen_wtr)
     sol_zen2 = asind(sind(sol_zen1(cos_indx))./1.34);   % Divide sin of theta by water refractive index (1.34)
     cos_solzen_wtr = cosd(sol_zen2);
     
-    % day_lngth = utc_h_from_midnght(sun_indx(1))-utc_h_from_midnght(sun_indx(2));
-    % sunset = utc_h_from_midnght(sun_indx(1));
-    % sunrise = utc_h_from_midnght(sun_indx(2));
-    
-    % Optional to plot cos_sEl vs utc_h_from_midnght
+    % Optional to plot cos_solzen_air vs utc_h_from_midnght
     % clf
     % plot(utc_h_from_midnght(cos_solzen_air>0),cos_solzen_wtr,'bo')
     
-    % Adjust irradiance values by cos_sE1, referencing time surface
+    % Adjust irradiance values by cos_solzen_wtr, referencing time surface
     % irradiance measurement was acquired
     
     % Find time closest to irradiance profile
     [~,~,~,h,mi,s] = datevec(utc);
     pro_hr = h; % + mi/60 + s/3600;
-    % pro_tm = floor(pro_hr);
-    % if isnan(pro_tm)
-    %     continue;
-    % end
     
-    % Get cos_sEl for irradiance profile time
+    % Get cos_solzen_wtr for irradiance profile time
     cos_solzen_pro_tm = cos_solzen_wtr(hour(mDatetm(cos_indx))==pro_hr);
     cos_solzen_ratio = cos_solzen_wtr./cos_solzen_pro_tm;
     
     %% Calculate light profiles
     
     % Approximate value for Ed/Eo
-    avg_cos = 0.8;
+    avg_cos = 0.8; % Assumed value for converting planar to scalar irradiance
     
-    % Load light profile data
-    ed_calculation_daily_extrapolation_with_satellite_input;
+    % Call script to compute irradiance profile data
+    ed_calculation_daily_extrapolation_with_satellite_input_git;
     
-    % % Get aph slope?
+    % Get aph slope and determine mean 
     % 
-    % if log(abs(aph_slope_var))>-8.3
+    % if log(abs(aph_slope_var))>-8.3  % Approach based on Verma et al. (2021)
     %     PUR_z=PUR_z_micro; % mol Q or mol photons m-2 h-1
-    %     aph440_mean = mean(aph440_micro);
-    % else 
-    %     PUR_z=PUR_z_pico; % mol Q or mol photons m-2 h-1
-    %     aph440_mean = mean(aph440_pico);
+    % else based
+    %     PUR_z=PUR_z_piconano; % mol Q or mol photons m-2 h-1
     % end
     
     %% Productivity Calculations
-    % Get indices for micro- and pico-dominated stations (based on aph440
-    % threshold)
+    % Get indices for micro- and piconano-dominated stations (based on aph440 threshold; see Hirata et al., 2008)
     % micro_indx = find(aph_rshp>0.1);
-    % pico_indx = find(aph_rshp<=0.1);
+    % piconano_indx = find(aph_rshp<=0.1);
     
-    % Alternatively use aph slope from PACE QAA aph product
+    % Alternatively use aph slope from PACE or MODIS QAA aph product
     micro_indx = find(log(abs(coeffs(2,:)))>-8.3);
-    pico_indx = find(log(abs(coeffs(2,:)))<=-8.3);
+    piconano_indx = find(log(abs(coeffs(2,:)))<=-8.3);
     
-    % Estimate aph440 normalized pmax (pmax_aph440_spec_est) and quantum yield
-    % (phi) (Based on results from pmax_box_plot.m)
+    % Estimate aph440 normalized pmax (pmax_aph440_spec_est) and quantum
+    % yield (Based on results from Mili and Lohrenz, 2026; see pmax_box_plot.m)
     pmax_aph440_spec_est = 0.0217*(1 - exp(-5.69*kd_rshp));   % mol C m-2 h-1
 
-    % Estimated phi based on correlation with pmax_aph440_spec (see pmax_aph_spec_vs_phi.m)
+    % Estimated phi based on correlation with pmax_aph440_spec ((Based on results from Mili and Lohrenz, 2026; see  pmax_aph_spec_vs_phi.m)
     phi_est = -60.3.*pmax_aph440_spec_est.^2 + 5.17.*pmax_aph440_spec_est - 0.00354;
-    % (Based on correlation between pmax_aph440_spec and phi in
-    %           'PEcurves_GC-phyto_allvar_revised_051624.xlsx')
     
     avg_cos = 0.8;  % Estimated value based on literature
     
-    % Estimate standard P-E parameters
+    % Estimate standard P-E par using relationships in Mili and Lohrenz(2026)
     alpha_est = zeros(size(aph_rshp));
     pbmax_est = pmax_aph440_spec_est.*aph_rshp./chl_rshp;  % mol C m-2 h-1 mg Chl-1
     alpha_est(micro_indx) = phi_est(micro_indx).*mean((aph_rshp(micro_indx)./chl_rshp(micro_indx)).*fmicro_aph_shape_interp,2);
-    alpha_est(pico_indx) = phi_est(pico_indx).*mean((aph_rshp(pico_indx)./chl_rshp(pico_indx)).*fpico_aph_shape_interp,2);
-    
-    % P_z = aph_440.*pmax_aph440_spec.*(1 - exp(-aph440_mean.*phi.*(PUR_z./avg_cos)./(aph_440.*pmax_aph440_spec))); % mol C m-3 h-1
-    % P_z_PAR = chl.*pbmax.*(1 - exp(-alpha.*(PAR_z_constK./avg_cos)'./pbmax))./12000; % mol C m-3 h-1
+    alpha_est(piconano_indx) = phi_est(piconano_indx).*mean((aph_rshp(piconano_indx)./chl_rshp(piconano_indx)).*fpiconano_aph_shape_interp,2);
     
     disp('Beginning productivity calculations...');
     
     % Run pp algorithm on profile for each hour, adjusting irradiance
-    % for cosine of solar zenith angle
+    % for cosine of solar zenith angle (Based on Mili and Lohrenz, 2026)
     
     clear P_z_est_day P_z_PAR_day
     
@@ -227,18 +213,19 @@ for mnthn = monthrange
         P_z_est(micro_indx,:) = aph_rshp(micro_indx).*pmax_aph440_spec_est(micro_indx).*...
             (1 - exp(-phi_est(micro_indx).*(PUR_z_micro(micro_indx,:,icos)./avg_cos)./(pmax_aph440_spec_est(micro_indx)))); % mol C m-3 h-1
         
-        % Calculate PP for pico-dominated stations (mol C m-3 h-1)
-        P_z_est(pico_indx,:) = aph_rshp(pico_indx).*pmax_aph440_spec_est(pico_indx).*...
-            (1 - exp(-phi_est(pico_indx).*(PUR_z_piconano(pico_indx,:,icos)./avg_cos)./(pmax_aph440_spec_est(pico_indx)))); % mol C m-3 h-1
+        % Calculate PP for piconano-dominated stations (mol C m-3 h-1)
+        P_z_est(piconano_indx,:) = aph_rshp(piconano_indx).*pmax_aph440_spec_est(piconano_indx).*...
+            (1 - exp(-phi_est(piconano_indx).*(PUR_z_piconano(piconano_indx,:,icos)./avg_cos)./(pmax_aph440_spec_est(piconano_indx)))); % mol C m-3 h-1
     
-        P_z_std = chl_rshp.*pbmax_est.*(1 - exp(-alpha_est.*(PAR_z(:,:,icos)./avg_cos)./pbmax_est))./12000; % mol C m-3 h-1
+        % Standard wavelength-integrated, PAR-based algorithm for comparison
+        % P_z_std = chl_rshp.*pbmax_est.*(1 - exp(-alpha_est.*(PAR_z(:,:,icos)./avg_cos)./pbmax_est))./12000; % mol C m-3 h-1
     
         if exist('P_z_est_day','var')
             P_z_est_day = P_z_est_day + P_z_est.*hr_incr;
-            % P_z_PAR_day = P_z_PAR_day + P_z_PAR;
+            % P_z_std_day = P_z_std_day + P_z_std;
         else
             P_z_est_day = P_z_est.*hr_incr;
-            % P_z_PAR_day = P_z_PAR;
+            % P_z_std_day = P_z_std;
         end
     
     end
